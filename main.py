04ab06e93efc6d341973c9e71aa43d3da0a8180c
dibaro <dibaroali@gmail.com>
2023-06-13 22:23:28 +0330

    adding input checker to app

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-from random import choice , shuffle
+from random import choice
 from time import sleep
 
-default="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+small="qwertyuiopasdfghjklzxcvbnm"
+cap="QWERTYUIOPASDFGHJKLZXCVBNM"
+numbers="1234567890"
+bajs="./!@#$%&*()_+"
+password=""
 
 password_length=input("welcome to the app.please give me your password length: ")
-if password_length.isnumeric:
+how_hard=input("how much do you want it complex? from 1 to 4 ? ")
+
+if password_length.isdigit() and how_hard.isnumeric() and int(how_hard)<=4:
+    if how_hard == "4":
+        final_str = small + cap + numbers + bajs
+    elif how_hard == "3":
+        final_str = small + cap + numbers 
+    elif how_hard == "2":
+        final_str = small + cap
+    else:
+        final_str = small
+
     print("i am generating password ")
-    sleep(3)
-    password=""
+
+    for _ in range (3):
+        print("*")
+        sleep(1)
+        
+
     for i in range(int(password_length)):
-        password+= choice(default)
+        password+= choice(final_str)
     print(f"here is your password = {password}")
 
     
 else :
-    print ("incorrect length")
+    print ("incorrect input")
